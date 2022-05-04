9905914f3860b6df49ff546287d3bf5b0dd40e0c
Simone Pavone <simonebest56@gmail.com>
2022-05-04 16:09:24 +0200

    problemi con get

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+$paragrafo=" Ciao, come stai? "
+//$parametro1 = $_GET['']
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    
+
+<p>
+
+<?php echo $paragrafo;
+ $paragrafoDopoTrim = trim ($paragrafo);
+?>
+
+</p>
+
+<p>
+    La lunghezza del paragrafo prima del trim è: <?php echo strlen($paragrafo);?>
+</p>
+
+<p>
+    La lunghezza del paragrafo dopo il trim è: <?php echo strlen($paragrafoDopoTrim);?>
+</p>
+
+<form methods="GET">
+    <input type="text" name="parametro1">
+    <button type="submit">INVIA</button>
+    <?php echo str_replace('come', '***', $paragrafo);
+    
+    ?>
+</form>
+
+</body>
+</html>
